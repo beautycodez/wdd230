e5a73f433c73836fd3313e036bfedb0429393815
beautycodez <angellorl_94@hotmail.com>
2023-04-17 07:42:52 -0500

    first task completed

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+let lastModi = new Date(document.lastModified);
+const day = lastModi.getDate();
+const month = lastModi.getMonth();
+const year = lastModi.getFullYear();
+const hour = lastModi.getHours();
+const min = lastModi.getMinutes();
+const sec = lastModi.getSeconds();
+const pickFooter = document.querySelector('#lastmodified').innerHTML += `Last Updated: ${month + 1}/${day}/${year} ${hour}:${min}:${sec}`;
+
