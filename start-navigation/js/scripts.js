12c210ce8f0474a91ac173000df89b00399d8abd
beautycodez <angellorl_94@hotmail.com>
2023-05-10 00:20:49 -0500

    hamburger btn

---
+++ start-navigation/js/scripts.js
@@ -0,0 +1,10 @@
+
+
+function toggleMenu (){
+    document.querySelector("#primaryNav").classList.toggle("open");
+    document.querySelector("#hamburgerBtn").classList.toggle("open");
+}
+
+const x = document.querySelector("#hamburgerBtn");
+x.onclick = toggleMenu;
+
