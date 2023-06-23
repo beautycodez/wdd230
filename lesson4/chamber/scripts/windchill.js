a8fd1e0115c751bf65309af08dd89837fee68c03
beautycodez <angellorl_94@hotmail.com>
2023-06-23 00:31:31 -0500

    sadadsf

--- lesson4/chamber/scripts/windchill.js
+++ lesson4/chamber/scripts/windchill.js
@@ -1,17 +1,55 @@
-const temperature = document.querySelector("#temperature");
 const windspeed = document.querySelector("#windspeed");
 const windChillElement = document.querySelector("#windchill");
 
-var tempValue = 50;
-var wsValue = 20;
+// ---------------------API-------------------------
+// select HTML elements in the document
+const currentTemp = document.querySelector('#temperature');
+const weatherIcon = document.querySelector('#weather-icon');
+const captionDesc = document.querySelector('figcaption');   
 
-const windChillValue = 35.74 + (0.6215 * tempValue) - (35.75 * (wsValue ** 0.16)) + (0.4275 * tempValue * (wsValue ** 0.16));
+const url = "https://api.openweathermap.org/data/2.5/weather?q=Lima&appid=f093ffd9fe067670b40d9ac069415766&units=imperial"
 
-temperature.innerHTML = `${tempValue} °F`;
-windspeed.innerHTML = `${wsValue}m/h`;
+async function apiFetch() {
+    try {
+      const response = await fetch(url);
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data); // this is for testing the call
+        displayResults(data);
+      } else {
+          throw Error(await response.text());
+      }
+    } catch (error) {
+        console.log(error);
+    }
+  }
+  
+apiFetch();
 
-if (tempValue <= 50 && wsValue >=3){
+function displayResults (weatherData){
+currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)} F°</strong>`;
+windspeed.innerHTML = `<strong>${weatherData.wind.speed} m/h</strong>`;
+const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
+const desc = weatherData.weather[0].description;
+
+const windChillValue = 35.74 + (0.6215 * weatherData.main.temp.toFixed(0)) - (35.75 * (weatherData.wind.speed ** 0.16)) + (0.4275 * weatherData.main.temp.toFixed(0) * (weatherData.wind.speed ** 0.16));
+if (currentTemp <= 50 && windspeed >=3){
     windChillElement.innerHTML = `${windChillValue.toFixed(2)}°F`;
 }else {
     windChillElement.innerHTML = "N/A";
 }
+
+weatherIcon.setAttribute('src', iconsrc);
+weatherIcon.setAttribute('alt', desc);
+captionDesc.textContent = desc;
+}
+// ---------------------END-------------------------
+
+var tempValue = 50;
+// var wsValue = weatherData.wind.speed.toFixed(0);
+
+
+
+temperature.innerHTML = `${tempValue} °F`;
+windspeed.innerHTML = `${wsValue}m/h`;
+
