a1d9fa7549d911720b0706fa7e328a5d4e4d4e36
beautycodez <angellorl_94@hotmail.com>
2023-06-27 19:48:12 -0500

    sdfsd

--- joyeria-neferet/neferet/scripts/anillos.js
+++ joyeria-neferet/neferet/scripts/anillos.js
@@ -1,5 +1,6 @@
+const muestrarioAnillos = document.querySelector(".muestrario-anillos");
 
-const url = "https://api.openweathermap.org/data/2.5/weather?q=Lima&appid=f093ffd9fe067670b40d9ac069415766&units=imperial"
+const url = "https://beautycodez.github.io/wdd230/joyeria-neferet/neferet/data/data.json"
 
 async function apiFetch() {
     try {
@@ -7,8 +8,9 @@ async function apiFetch() {
       if (response.ok) {
         const data = await response.json();
         console.log(data); // this is for testing the call
-        displayResults(data);
+        displayResults(data.anillos);
       } else {
+        console.log("not work")
           throw Error(await response.text());
       }
     } catch (error) {
@@ -18,20 +20,26 @@ async function apiFetch() {
   
 apiFetch();
 
-function displayResults (weatherData){
-currentTemp.innerHTML = `<strong>${weatherData.main.temp.toFixed(0)} F°</strong>`;
-windspeed.innerHTML = `<strong>${weatherData.wind.speed} m/h</strong>`;
-const iconsrc = `https://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`;
-const desc = weatherData.weather[0].description;
+function displayResults (data){
 
-const windChillValue = 35.74 + (0.6215 * weatherData.main.temp.toFixed(0)) - (35.75 * (weatherData.wind.speed ** 0.16)) + (0.4275 * weatherData.main.temp.toFixed(0) * (weatherData.wind.speed ** 0.16));
-if (currentTemp <= 50 && windspeed >=3){
-    windChillElement.innerHTML = `${windChillValue.toFixed(2)}°F`;
-}else {
-    windChillElement.innerHTML = "N/A";
-}
+        data.forEach((anillo) =>{
+            const sectionElement = document.createElement("section");
 
-weatherIcon.setAttribute('src', iconsrc);
-weatherIcon.setAttribute('alt', desc);
-captionDesc.textContent = desc;
+                const h1Element = document.createElement("h1");
+                const pictureElement = document.createElement("picture");
+                    const imgElement = document.createElement("img");
+
+            imgElement.setAttribute("src",anillo.img);
+            imgElement.setAttribute("alt", "my ring");
+
+            h1Element.innerHTML = `<strong>Anillo número ${anillo.numero}`
+
+
+            pictureElement.appendChild(imgElement);
+            sectionElement.appendChild(h1Element);
+            sectionElement.appendChild(pictureElement);
+
+            muestrarioAnillos.appendChild(sectionElement);
+        })
+        
 }
